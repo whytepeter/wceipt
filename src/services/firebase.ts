72966f2283d50f8f2c4460320faff58b97add129
Whyte Peter <peteremmanuelwhyte@gmail.com>
2023-12-08 20:27:00 +0100

    added the firebase config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,7 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  // ...
+  apiKey: "AIzaSyCG-L8reMXHBs0fwexuarLUsnWcTgL3Qx8",
+  authDomain: "wceipt.firebaseapp.com",
+  projectId: "wceipt",
+  storageBucket: "wceipt.appspot.com",
+  messagingSenderId: "986221768619",
+  appId: "1:986221768619:web:cc48b9dd9dbf8cb5302cee",
 };
 
 const app = initializeApp(firebaseConfig);
