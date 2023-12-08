0cb95214ad083e389fdd259e7f38a171461bf757
Whyte Peter <peteremmanuelwhyte@gmail.com>
2023-12-08 21:41:00 +0100

    fixed tailwind not working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ReduxProvider } from "@/redux/provider";
 import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,10 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <ReduxProvider>
-        <div>{children}</div>
-      </ReduxProvider>
-    </>
+    <html lang="en">
+      <body>
+        <ReduxProvider>{children}</ReduxProvider>
+      </body>
+    </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main>Hello</main>;
+  return (
+    <>
+      <div className="bg-[#F4D690] text-2xl w-screen h-screen p-4">HOME</div>
+    </>
+  );
 }
