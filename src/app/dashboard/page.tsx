a73c99fa7decde6af7f70d68b818739104be4fb8
Whyte Peter <peteremmanuelwhyte@gmail.com>
2023-12-08 09:23:48 +0100

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import React from "react";
+
+export default function Dashboard() {
+  return <div>Dashboard</div>;
+}
---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const isPublicPath = path == "/" || path == "/auth/:path*";
+  const token = request.cookies.get("token")?.value || "";
+
+  // if (isPublicPath && token) {
+  //   return NextResponse.redirect(new URL("/", request.nextUrl));
+  // }
+  // if (!isPublicPath && !token) {
+  //   return NextResponse.redirect(new URL("/login", request.nextUrl));
+  // }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/", "/auth/:path*", "/dashboard/:path*", "/admin/:path*"],
+};
