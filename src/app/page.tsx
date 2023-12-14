8825a8e2e6200c040566c078218cc5a4a49bea33
Isaiah Ernest Ovie <officialisaiahovie@gmail.com>
2023-12-14 19:41:09 +0100

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-"use client";
-import WButton from "@/components/global/WButton";
-import { Button, styled } from "@mui/material";
+'use client';
+import WButton from '@/components/global/WButton';
+import { Button, styled } from '@mui/material';
 
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
@@ -16,7 +16,7 @@ export default function Home() {
 
   return (
     <>
-      <div className="bg-[#F4D690] text-2xl w-screen h-screen p-4 ">
+      <div className='bg-[#F4D690] text-2xl w-screen h-screen p-4 '>
         Home
         <WButton onClick={handleClick} loading={loading}>
           CLICK ME
