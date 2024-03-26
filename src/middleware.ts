a28b25f9a612ac2f19f87468e4130a05d99d5842
Whyte Peter <peteremmanuelwhyte@gmail.com>
2024-03-26 09:59:52 +0100

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,15 +7,23 @@ export function middleware(request: NextRequest) {
   const isPublicPath = path == "/" || path == "/auth/:path*";
   const isLoggedIn = true; // get logged in user
 
-  const state = store.getState();
-  console.log(state);
-
-  if (!isPublicPath && !isLoggedIn) {
+  const cookies = request.cookies;
+  const token = cookies.get("token");
+  if (!token)
     return NextResponse.redirect(new URL("/auth/login", request.nextUrl));
-  }
+
+  const decoded = jwtDecode(token.value);
+  const now = new Date().getTime();
+  const expiryDate = (decoded.exp as number) * 1000;
+
+  const tokenExpired = now > expiryDate;
+  if (tokenExpired)
+    return NextResponse.redirect(new URL("/auth/login", request.nextUrl));
+
+  return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/", "/auth/:path*", "/dashboard/:path*", "/admin/:path*"],
+  matcher: ["/dashboard/:path*", "/admin/:path*"],
 };
